5820cca4f791e3919e5df60d8183e77d1ecaa4a2
cdkakade <cdkakade1@gmail.com>
2024-09-13 18:52:27 +0530

    Added service level security

--- src/main/java/expense/manager/currency/McroCurrencyServiceApplication.java
+++ src/main/java/expense/manager/currency/McroCurrencyServiceApplication.java
@@ -2,10 +2,12 @@ package expense.manager.currency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ComponentScan(basePackages = {"expense.manager.common", "expense.manager.currency"})
 public class McroCurrencyServiceApplication {
 
 	public static void main(String[] args) {
